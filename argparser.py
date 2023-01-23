2d77fcc52df7f4d3dbff78485436d1abaaafbd26
masonrware <86129546+masonrware@users.noreply.github.com>
2023-01-23 15:40:02 -0600

    finished argparser

--- argparser.py
+++ argparser.py
@@ -8,7 +8,7 @@
 
 # This is an argument parser to serve as a substitute for pythons argparse.
 # It is also heavily influenced in architecture and naming convention by the aforementioned library.
-# It works to expect user-configured arguments and, based on the user's actual cli args, 
+# It works to expect user-configured arguments and, based on the user's actual cli args,
 # returns an object with boolean attributes corresponding to each user-configured arg.
 
 from ast import List
@@ -30,75 +30,87 @@ class Argument:
         self.param = param
         self.description = description
 
-    def __eq__ (self, other):
+    def __eq__(self, other):
         return self.name == other.name
-    
-    def __nq__ (self, other):
+
+    def __nq__(self, other):
         return self.name != other.name
 
     def __hash__(self):
         return hash(self.name)
-    
+
     def __str__(self):
         return f"Argument: {self.name}"
 
+
 class Namespace:
-    def __init__(self, *args):
-        for idx, item in enumerate(args):
-            setattr(self, f"{idx}", item)
+    def __init__(self, args):
+        for item in args:
+            setattr(self, f"{item.name}", item.param)
+
 
 class ArgumentParser:
     description: str = ""
     targetFile: str = ""
     known_args: Set = set()
 
-    def __init__(self, description: str="", target: str=""):
+    def __init__(self, description: str = "", target: str = ""):
         self.description = description
         self.targetFile = target
 
     def add_argument(
         self, kwarg: str, help: str = "", param: bool = False, description: str = ""
     ):
-        self.known_args.add(Argument(kwarg=kwarg, help=help, param=param, description=description))
+        self.known_args.add(
+            Argument(kwarg=kwarg, help=help, param=param, description=description)
+        )
 
     # check runtime arguments (user_args) against known args and create namespace
     def _create_namespace(self, user_args: List):
         true_args: Set = set()
-        
+        namespace_args: List = list()
+
         # replace each user_arg string with an argument object for comparison to known arguments
         for idx, user_arg in enumerate(user_args):
             user_args[idx] = Argument(user_arg)
-        
+
         user_args = set(user_args)
-        
+
         # implemented Set.Union method because union wasn't working
         for known_arg in self.known_args:
             for user_arg in user_args:
                 # use built in __eq__
                 if user_arg == known_arg:
                     true_args.add(known_arg)
-                
-        # check if there are any unknown/invalid arguments 
+
+        # check if there are any unknown/invalid arguments
         invalid_args = set()
         for user_arg in user_args:
             if user_arg not in true_args:
                 invalid_args.add(user_arg)
 
         # throw exception
-        if len(invalid_args)>0:
-            raise Exception(f"The following arguments are unknown/invalid: {', '.join(str(v) for v in invalid_args)}")
-                    
+        if len(invalid_args) > 0:
+            raise Exception(
+                f"The following arguments are unknown/invalid: {', '.join(str(v) for v in invalid_args)}"
+            )
+
         # TODO populate Namespace object
         # add all true arguments
-        
+        for true_arg in true_args:
+            true_arg.param = True
+            namespace_args.append(true_arg)
+
         # add all false arguments
-        
+        for known_arg in self.known_args:
+            if known_arg not in namespace_args:
+                known_arg.param = False
+                namespace_args.append(known_arg)
+
         # return created namespace object
-                
-        return set(user_args)
-    
+        return Namespace(namespace_args)
+
     def parse_args(self):
-        ## TODO add text to test.py
-                
-        self._create_namespace(sys.argv[1:])
-            
+        return self._create_namespace(sys.argv[1:])
+
+    ## TODO add text to test.py probably in the parser not here!!!!
--- test.py
+++ test.py
@@ -16,26 +16,13 @@ def reverse(string: str) -> str:
 
 
 if __name__ == "__main__":
-    # TODO build own argparse that can recognize different positional arguments?
-    # otherwise just give up and use argparse with flags ew
-
-    # ## for flags:
-    # parser = argparse.ArgumentParser(description="beta argument testing")
-
-    # parser.add_argument("print", nargs="*", help="print out mason")
-
-    # args = parser.parse_args()
-
-    # if args.print:
-    #     print("mason")
-
     parser = argparser.ArgumentParser("test custom arg parser")
 
     parser.add_argument("test")
     parser.add_argument("also")
-    
-    args = parser.parse_args()
-    
-    # from pprint import pprint
-    # pprint(vars(args))
 
+    args = parser.parse_args()
+
+    from pprint import pprint
+
+    pprint(vars(args))
